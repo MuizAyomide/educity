8bc3176a07aa6ce2984416629f5c1ff02dc6d326
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-03-12 02:00:01 +0100

    Initializing repo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 
 const Navbar = () => {
+
+const [sticky, setSticky] = useState(false);
+
+useEffect(()=>{
+  window.addEventlistener('scroll', ()=>{
+    window.scrollY > 50 ? setSticky(true) : setSticky(false);
+  })
+},{})
+
   return (
     <nav className='container'>
       <img src={logo} alt="" className='logo'/>
